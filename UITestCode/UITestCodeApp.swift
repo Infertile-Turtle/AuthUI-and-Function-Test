08ae6c2319b43892feebb8e8898f97866def01fc
Matthew Nos <mnos@cloudingenuity.com>
2023-01-25 09:29:18 -0600

    Initial Commit

---
+++ UITestCode/UITestCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestCodeApp.swift
+//  UITestCode
+//
+//  Created by Matthew Nos on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
